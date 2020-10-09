9364f7c40054080c58e87ff277d8ea34942a6c48
LEFEBVRE TIMOTHEE <tlefeb08@localhost.localdomain>
2020-10-09 09:39:19 +0200

    added manage users page

---
+++ admin/manage_users.php
@@ -0,0 +1,123 @@
+<?php include("../includes/mysql.php"); ?>
+<?php include("includes/checkperms.php"); ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Gestion des utilisateurs</title>
+    <!-- CSS only -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous" />
+
+    <!-- JS, Popper.js, and jQuery -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous">
+    </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="//cdn.datatables.net/1.10.22/js/jquery.dataTables.min.js"></script>
+    <link rel="stylesheet" href="//cdn.datatables.net/1.10.22/css/jquery.dataTables.min.css"/>
+    <link rel="stylesheet" href="../style/style.css" />
+    <script>
+        $(document).ready( function () {
+            $('#datatable').DataTable({
+                language: {
+                    url: 'https://cdn.datatables.net/plug-ins/1.10.21/i18n/French.json'
+                }
+            } );
+        });
+    </script>
+    <style>
+#row {
+  width: 100%;
+  text-align: center;
+}
+
+table {
+  margin: 0 auto;
+  width: 100%;
+  table-layout: fixed;
+}
+
+td {
+    width: 33.3%;
+    padding-right: 17px;
+}
+</style>
+
+
+</head>
+
+<body class="d-flex flex-column min-vh-100">
+    <!--Navbar-->
+    <?php include("includes/navbar.php"); ?>
+    <!--Fin navbar -->
+
+    <div class="container change-section">
+
+        <div class="row h-100 align-self-center">
+
+            <div class="col-md-12 text-center">
+
+                <h2 id="title-current-place" style="padding: 10px;">Panel de gestion</h2>
+
+
+                <div id="box">
+                    <h3>Gestion des utilisateurs</h3>
+                    <br>
+                    <table id="datatable" class="table table-striped table-bordered" width="100%">
+                                            <thead>
+                                                <tr>
+                                                    <th>ID</th>
+                                                    <th>Nom</th>
+                                                    <th>Prénom</th>
+                                                    <th>Email</th>
+                                                    <th>Rôle</th>
+                                                    <th>Actions</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+
+                                            <?php
+                                                $users = $db->query("SELECT * FROM USERS");
+                                                while ($u = $users->fetch()) {
+                                                    echo '<tr>
+                                                            <td>' . $u['id_user'] . '</td>
+                                                            <td>' . $u['surname'] . '</td>
+                                                            <td>' . $u['name'] . '</td>
+                                                            <td>' . $u['email'] . '</td>
+                                                            <td>' . $u['role'] . '</td>
+                                                            <td><a href="#" class="btn btn-dark">éditer</a></td>
+                                                            </tr>';
+
+                                                }
+                                            ?>
+                        </tbody>
+                      </table>
+                    <br><br>
+                    <a href="index.php" class="btn btn-dark">Retour à l'accueil</a>
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+
+
+    </div>
+
+
+
+
+
+    <!-- Footer -->
+    <?php include("../includes/footer.php"); ?>
+    <!-- Footer -->
+</body>
+
+</html>
