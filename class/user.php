a7766a91246a9da2cd04558f6aae85a1c7865e14
BASTOS ANTOINE <cydaw6@gmail.com>
2020-10-09 11:03:01 +0200

    debut classe user

---
+++ class/user.php
@@ -0,0 +1,29 @@
+<?php
+
+
+class User{
+
+    private $idUser;
+    private $name;
+    private $sessionId;
+
+    public function createUser($idUser, $name, $sessionId){
+        $this.$idUser = $idUser;
+        $this.$name = $name;
+        $this.$sessionId = $idUser;
+    }
+
+    public function authorized(){
+        
+        
+
+    }
+
+
+
+
+
+
+
+}
+?>
