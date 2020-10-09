ee47dd019507c462ec92c019cd68bff791383365
zkimi <zkimi0fficiel@gmail.com>
2020-10-09 12:06:16 +0200

    responsive datatable

--- admin/manage_users.php
+++ admin/manage_users.php
@@ -20,6 +20,7 @@
     </script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="//cdn.datatables.net/1.10.22/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/responsive/2.2.6/js/dataTables.responsive.min.js"></script>
     <link rel="stylesheet" href="//cdn.datatables.net/1.10.22/css/jquery.dataTables.min.css"/>
     <link rel="stylesheet" href="../style/style.css" />
     <script>
@@ -27,7 +28,8 @@
             $('#datatable').DataTable({
                 language: {
                     url: 'https://cdn.datatables.net/plug-ins/1.10.21/i18n/French.json'
-                }
+                },
+                responsive: true
             } );
         });
     </script>
