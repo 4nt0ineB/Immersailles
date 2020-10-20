e48d4190675e35bf8c6d7bd0c4657ed9e2f51ba3
cydaw6 <cydaw6@gmail.com>
2020-10-21 01:28:37 +0200

    remove error

--- admin/recovery.php
+++ admin/recovery.php
@@ -1,91 +1,122 @@
 <?php
-class User
-{
-    public static $db;
-    private $idUser;
-    private $name;
-    private $sessionId;
-    public function __construct($idUser)
-    {
-        $this->sessionId = "";
-        $this->idUser = $idUser;
-    }
-    public function getId()
-    {
-        return $this->idUser;
-    }
-    public function setSession($id)
-    {
-        $this->sessionId = $id;
-    }
-    public function connect()
-    {
-        /* associe le session_id de l'instance user, à son user correspondant dans la bdd */
-        User::$db->query("UPDATE USERS SET session_id = '$this->sessionId' WHERE id_user = '$this->idUser'");
-        return 1;
-    }
-    public function disconnect()
-    {
-        /* remet à NULL session_id de l'utilisateur */
-        User::$db->query("UPDATE USERS SET session_id = default WHERE id_user = '$this->idUser'");
-    }
-    public function refreshSession()
-    {
-        /* si l'id de la session dans la db est différent du session_id de l'user (bdd) on détruit la session. 
-        retourne 0 si déconnecté, 1 si non */
-        $result = User::$db->query("SELECT session_id FROM USERS WHERE id_user = $this->idUser")->fetch();
-        if ($result["session_id"] != $this->sessionId) {
-            header("refresh:0; logout.php");
-            return 0;
-        }
-        return 1;
-    }
-    public static function numberConnectedUsers()
-    {
-        /* Retourne le nombre d'utilisateurs ayant un session_id différent de NULL */
-        $r = User::$db->query("SELECT count(*) AS nb FROM USERS WHERE session_id != ''")->fetch();
-        return $r["nb"];
-    }
-    public static function sendTokenRecovery($mail)
-    {
-        /* Envoie un mail de recup mdp avec un lien avec un token dans le GET */
-        $r = User::$db->query("SELECT id_user FROM USERS WHERE email = \"$mail\"")->fetch(); // id de l'user pour le mail donnée
-        $id_user = $r["id_user"];
-        if (!empty($id_user)) {
-            $nowNCooldown = strtotime("-2 hour", strtotime(date("Y-m-d H:i:s"))); //heure actuelle - cooldown de 2h pour chaque nouveau token
-            $actualToken = User::$db->query("SELECT * FROM PSSWD_RECOVER WHERE date < '$nowNCooldown' AND id_user = $id_user ")->rowCount(); //on cherche les token dont le cooldown de 2h n'est pas expiré
-            if (($actualToken == 0)) {
-                $token = generateRandomString(40);
-                User::$db->query("INSERT INTO PSSWD_RECOVER VALUES(NULL, '$token', NOW(), $id_user)"); // def le token pour l'utilisateur
-                $to = $mail;
-                $from = 'no-reply@immersailles.me';
-                $fromName = 'no-reply';
-                $subject = "Récupération de mot de passe";
-                $htmlContent = ' 
-                    <html> 
-                    <head> 
-                        <meta charset="UTF-8" />
-                    </head>
-                    <body> 
-                        <h3 style="background-color: black; color: white;">Demande de récupération de mot de passe</h3> 
-                        <p>Vous avez effectué une demande de récupération de mot de passe. 
-                            <br>Si vous n\'êtes pas à l\'origine de cette demande ignorez ce mail .
-                        </p>
-                        <br>
-                        <p> Le lien de récupération ne sera valable que pendant deux heures.</p>
-                        <a href="https://immersailles.me/recovery.php?re=' . $token . '">réinitialiser mon mot de passe</a>
-                        <a href="https://immersailles.me/admin/recovery.php?re=' . $token . '">réinitialiser le mot de passe</a>
-                    </body> 
-                    </html>';
 
-                // Set content-type header for sending HTML email 
-                $headers = "MIME-Version: 1.0" . "\r\n";
-                $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
-                // Additional headers 
-                $headers .= 'From: ' . $fromName . '<' . $from . '>' . "\r\n";
-                // Send email 
-                return mail($to, $subject, $htmlContent, $headers);
-            }
-        }
-    }
-}
+require_once("../includes/mysql.php");
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Mot de passe perdu</title>
+    <?php require_once("includes/head.html") ?>
+</head>
+
+<body class="d-flex flex-column min-vh-100">
+    <!--Navbar-->
+    <nav class="navbar navbar-expand-lg navbar-dark sticky-top">
+        <div class="container">
+            <a class="navbar-brand" href="../"><img src="../img/logo_mini.png" /> <i>IMMERSAILLES</i></a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ml-auto text-center">
+                    <li class="nav-item">
+                        <a class="nav-link scroll" href="">A PROPOS <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a href=""><span class="dot"></span> </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href=""><span class="dot"></span></a>
+                    </li>
+
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <!--Fin navbar -->
+    <!--Connexion container-->
+    <div class="container change-section">
+        <div class="row"><br></div>
+        <div class="row"><br></div>
+        <div class="row"><br></div>
+        <div class="row"><br></div>
+        <div class="row"><br></div>
+
+        <div class="row">
+
+            <div class="col-lg text-center">
+
+                <h2 id="title-current-place" style="padding: 10px;">Réinitialiser le mot de passe</h2>
+
+
+                <div id="box">
+                    <form method="POST">
+                        <div class="form-group">
+                            <label for="exampleInputEmail1">Adresse email</label>
+                            <input type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Votre email">
+                        </div>
+                        <button type="submit" name="send" class="btn btn-primary">Envoyer</button>
+                    </form>
+                    <br>
+                    <?php
+
+                    if (isset($_POST["send"])) {
+                        $mail = $_POST["email"];
+
+                        $sender = 'noreply.immersailles@gmail.com';
+                        $recipient = 'antoine.ba6@gmail.com';
+
+                        $subject = "php mail test";
+                        $message = "php test message";
+                        $headers = 'From:' . $sender;
+
+                        if (mail($recipient, $subject, $message, $headers)) {
+                            echo "Message accepted";
+                        } else {
+                            echo "Error: Message not accepted";
+                        }
+
+
+                        if (User::sendTokenRecovery($mail)) {
+                    ?>
+                            <div class="alert alert-success" role="alert">
+                                Si vous possedez un compte un mail de récupération vous sera envoyé.
+                            </div>
+
+                        <?php
+                        } else {
+                        ?>
+                            <div class="alert alert-success" role="alert">
+                                Une erreur est survenue votre demande n'a pas pu être prise en compte.
+                            </div>
+
+                    <?php
+                        }
+                    }
+
+
+
+
+                    ?>
+                </div>
+            </div>
+        </div>
+
+
+
+    </div>
+
+
+
+    <!-- Footer -->
+    <?php include("../includes/footer.php"); ?>
+    <!-- Footer -->
+</body>
+
+</html>
--- class/user.php
+++ class/user.php
@@ -1,50 +1,38 @@
 <?php
-
-
 class User
 {
     public static $db;
-
     private $idUser;
     private $name;
     private $sessionId;
-
     public function __construct($idUser)
     {
         $this->sessionId = "";
         $this->idUser = $idUser;
     }
-
     public function getId()
     {
         return $this->idUser;
     }
-
     public function setSession($id)
     {
         $this->sessionId = $id;
     }
-
     public function connect()
     {
         /* associe le session_id de l'instance user, à son user correspondant dans la bdd */
-
         User::$db->query("UPDATE USERS SET session_id = '$this->sessionId' WHERE id_user = '$this->idUser'");
         return 1;
     }
-
     public function disconnect()
     {
         /* remet à NULL session_id de l'utilisateur */
-
         User::$db->query("UPDATE USERS SET session_id = default WHERE id_user = '$this->idUser'");
     }
-
     public function refreshSession()
     {
         /* si l'id de la session dans la db est différent du session_id de l'user (bdd) on détruit la session. 
         retourne 0 si déconnecté, 1 si non */
-
         $result = User::$db->query("SELECT session_id FROM USERS WHERE id_user = $this->idUser")->fetch();
         if ($result["session_id"] != $this->sessionId) {
             header("refresh:0; logout.php");
@@ -52,40 +40,27 @@ class User
         }
         return 1;
     }
-
     public static function numberConnectedUsers()
     {
         /* Retourne le nombre d'utilisateurs ayant un session_id différent de NULL */
-
         $r = User::$db->query("SELECT count(*) AS nb FROM USERS WHERE session_id != ''")->fetch();
         return $r["nb"];
     }
-
     public static function sendTokenRecovery($mail)
     {
         /* Envoie un mail de recup mdp avec un lien avec un token dans le GET */
-
-
         $r = User::$db->query("SELECT id_user FROM USERS WHERE email = \"$mail\"")->fetch(); // id de l'user pour le mail donnée
         $id_user = $r["id_user"];
-
-
-
         if (!empty($id_user)) {
-
             $nowNCooldown = strtotime("-2 hour", strtotime(date("Y-m-d H:i:s"))); //heure actuelle - cooldown de 2h pour chaque nouveau token
             $actualToken = User::$db->query("SELECT * FROM PSSWD_RECOVER WHERE date < '$nowNCooldown' AND id_user = $id_user ")->rowCount(); //on cherche les token dont le cooldown de 2h n'est pas expiré
             if (($actualToken == 0)) {
-
                 $token = generateRandomString(40);
                 User::$db->query("INSERT INTO PSSWD_RECOVER VALUES(NULL, '$token', NOW(), $id_user)"); // def le token pour l'utilisateur
-
                 $to = $mail;
                 $from = 'no-reply@immersailles.me';
                 $fromName = 'no-reply';
-
                 $subject = "Récupération de mot de passe";
-
                 $htmlContent = ' 
                     <html> 
                     <head> 
@@ -99,16 +74,15 @@ class User
                         <br>
                         <p> Le lien de récupération ne sera valable que pendant deux heures.</p>
                         <a href="https://immersailles.me/recovery.php?re=' . $token . '">réinitialiser mon mot de passe</a>
+                        <a href="https://immersailles.me/admin/recovery.php?re=' . $token . '">réinitialiser le mot de passe</a>
                     </body> 
                     </html>';
 
                 // Set content-type header for sending HTML email 
                 $headers = "MIME-Version: 1.0" . "\r\n";
                 $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
-
                 // Additional headers 
                 $headers .= 'From: ' . $fromName . '<' . $from . '>' . "\r\n";
-
                 // Send email 
                 return mail($to, $subject, $htmlContent, $headers);
             }
