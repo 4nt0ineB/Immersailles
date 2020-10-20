477d94278dfd5d19d750a5e46dc89ddfde23a1b4
cydaw6 <cydaw6@gmail.com>
2020-10-21 01:35:39 +0200

    -

--- class/user.php
+++ class/user.php
@@ -52,9 +52,8 @@ class User
         $r = User::$db->query("SELECT id_user FROM USERS WHERE email = \"$mail\"")->fetch(); // id de l'user pour le mail donnée
         $id_user = $r["id_user"];
         if (!empty($id_user)) {
-            $dat = date("Y-m-d H:i:s");
-            $nowNCooldown = strtotime("-2 hour", strtotime($dat)); //heure actuelle - cooldown de 2h pour chaque nouveau token
-            $actualToken = User::$db->query("SELECT * FROM PSSWD_RECOVER WHERE id_user = $id_user AND date BETWEEN '$nowNCooldown' AND '$dat'")->rowCount(); //on cherche les token dont le cooldown de 2h n'est pas expiré
+            $nowNCooldown = strtotime("-2 hour", strtotime(date("Y-m-d H:i:s"))); //heure actuelle - cooldown de 2h pour chaque nouveau token
+            $actualToken = User::$db->query("SELECT * FROM PSSWD_RECOVER WHERE date < '$nowNCooldown' AND id_user = $id_user ")->rowCount(); //on cherche les token dont le cooldown de 2h n'est pas expiré
             if (($actualToken == 0)) {
                 $token = generateRandomString(40);
                 User::$db->query("INSERT INTO PSSWD_RECOVER VALUES(NULL, '$token', NOW(), $id_user)"); // def le token pour l'utilisateur
