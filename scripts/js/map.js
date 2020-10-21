1552fd8ff789c65f5cf95de53a4c34e93b054711
Zkimi <20417186+zkimi@users.noreply.github.com>
2020-10-21 19:00:34 +0200

    Marker icon on map

--- scripts/js/map.js
+++ scripts/js/map.js
@@ -1,7 +1,8 @@
 var map = L.map('mapid', {
             crs: L.CRS.Simple,
             zoom: -1.8,
-            minZoom:-1.8
+            minZoom:-1.8,
+            maxZoom:1
         });
 
     map.setMaxBounds(new L.LatLngBounds([2319,0], [0,6507]));
@@ -15,6 +16,18 @@ var map = L.map('mapid', {
 	map.on('click', function(e) {
     console.log("Lat, Lon : " + e.latlng.lat + ", " + e.latlng.lng);
 });
+
+	var immersaillesIcon = L.icon({
+    iconUrl: '././img/marker.png',
+
+    iconSize:     [38, 50],
+    iconAnchor: [38/2,50],
+    popupAnchor:  [0, -40] // point from which the popup should open relative to the iconAnchor
+});
+
+	L.marker([1173.5021593231754, 2687.9863590540663], {icon: immersaillesIcon}).addTo(map).bindPopup("Grande salle des gardes");
+
+	
 
 
         var div = L.DomUtil.get('noscroll');
