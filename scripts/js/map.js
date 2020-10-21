beee99878d4bc233d66d11e1cf2d9b65847da0fb
Zkimi <20417186+zkimi@users.noreply.github.com>
2020-10-21 18:45:40 +0200

    PLAN 1 LEAFLET OK

--- scripts/js/map.js
+++ scripts/js/map.js
@@ -1,14 +1,21 @@
 var map = L.map('mapid', {
-            center: [50.99, 10.191],
-            zoom: 6,
-            minZoom:6,
-            maxBounds:[[55.3915921070334,20.610351562500004],[47.17477833929906,1.7138671875000002]]
+            crs: L.CRS.Simple,
+            zoom: -1.8,
+            minZoom:-1.8
         });
 
+    map.setMaxBounds(new L.LatLngBounds([2319,0], [0,6507]));
 
 
-        var basemap = L.tileLayer("https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png");
-        basemap.addTo(map);
+    var bounds = [[0,0], [2319,6507]];
+	var image = L.imageOverlay('http://localhost/Immersailles/admin/upload/plan_1735.png', bounds).addTo(map);
+
+	map.fitBounds(bounds);
+
+	map.on('click', function(e) {
+    console.log("Lat, Lon : " + e.latlng.lat + ", " + e.latlng.lng);
+});
+
 
         var div = L.DomUtil.get('noscroll');
   L.DomEvent.on(div, 'mousewheel', L.DomEvent.stopPropagation);
