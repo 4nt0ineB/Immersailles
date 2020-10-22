8ac8ee910470daabc0d866e0cbb600d0261b8c89
cydaw6 <cydaw6@gmail.com>
2020-10-22 12:27:41 +0200

    -

--- admin/recovery.php
+++ admin/recovery.php
@@ -97,8 +97,8 @@ if (isset($_SESSION["user"])) {
                                 $getTokendate = $db->query("SELECT date FROM PSSWD_RECOVER where token=\"$token\"")->fetch();
                                 echo date("Y-m-d H:i:s", strtotime("+1 hour", strtotime($getTokendate["date"])));;
                                 $tokenDateNcooldown = date("Y-m-d H:i:s", strtotime("+1 hour", strtotime($getTokendate["date"])));
-                                echo date("Y-m-d H:i:s") . " >" . $getTokendate;
-                                if ($getTokendate <= strtotime(date("Y-m-d H:i:s"))) { //date token + 1h <= maintenant
+                                echo date("Y-m-d H:i:s") . " >" . date("Y-m-d H:i:s", $getTokendate);
+                                if ($getTokendate <= date("Y-m-d H:i:s")) { //date token + 1h <= maintenant
                                     $existUser = $db->query("SELECT id_user FROM PSSWD_RECOVER WHERE token=\"$token\" AND state=0")->fetch();
                                     $existUser = $existUser["id_user"];
                                 }
