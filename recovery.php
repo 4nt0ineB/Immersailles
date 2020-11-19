4712480c38e1cd83b8ccb733a35c48af736d3bd2
cydaw6 <cydaw6@gmail.com>
2020-11-19 14:53:24 +0100

    path to image correction

--- recovery.php
+++ recovery.php
@@ -38,7 +38,7 @@ if (isset($_SESSION["user"])) {
     <!--Navbar-->
     <nav class="navbar navbar-expand-lg navbar-dark sticky-top">
         <div class="container">
-            <a class="navbar-brand" href="../"><img src="../img/logo_mini.png" /> <i>IMMERSAILLES</i></a>
+            <a class="navbar-brand" href="./"><img src="./img/logo_mini.png" /> <i>IMMERSAILLES</i></a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
