c3d598fdc05125124aca7030ef23ef130622cea3
Zkimi <20417186+zkimi@users.noreply.github.com>
2020-11-20 09:21:11 +0100

    remove selected attribute timeline

--- index.php
+++ index.php
@@ -252,11 +252,11 @@
                 <div class="events-wrapper">
                     <div class="events">
                         <ol style="list-style-type: none;">
-                        
+
                             <li><a href="#0" onclick="changeMapByYear(1631)" data-date="00/00/1635" class="selected">1635</a></li>
-                            <li><a href="#0" data-date="00/00/1789" class="selected">1789</a></li>
-                            <li><a href="#0" data-date="00/00/1820" class="selected">1820</a></li>
-                            <li><a href="#0" data-date="00/00/1950" class="selected">1950</a></li>
+                            <li><a href="#0" data-date="00/00/1789">1789</a></li>
+                            <li><a href="#0" data-date="00/00/1820">1820</a></li>
+                            <li><a href="#0" data-date="00/00/1950">1950</a></li>
 
                         </ol>
 
